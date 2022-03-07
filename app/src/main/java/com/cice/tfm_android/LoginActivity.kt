0b4a9c5f6dd340fe004aa303b24781ba04a15c52
Pablozzz19 <72037798+Pablozzz19@users.noreply.github.com>
2022-03-07 18:36:19 +0100

    [WIP] - Validate Email and Password

--- app/src/main/java/com/cice/tfm_android/LoginActivity.kt
+++ app/src/main/java/com/cice/tfm_android/LoginActivity.kt
@@ -2,16 +2,59 @@ package com.cice.tfm_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import com.cice.tfm_android.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    // region - Global Variables
+    private lateinit var binding: ActivityLoginBinding
+    // endregion
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignIn.setOnClickListener {
+            addingErrorsTextField()
+        }
+
     }
 
     // region - Private Methods
-    private fun AddingErrorsTextField() {
+    private fun addingErrorsTextField() {
 
+        val etEmail = binding.etLoginEmail.text.toString()
+        val etPsw = binding.etLoginPsw.text.toString()
+
+        if (etEmail != null) {
+            when {
+                etEmail.isEmpty() -> {
+                    // Set error text
+                    binding.tilLoginEmail.error = "isEmpty"
+                }
+                Patterns.EMAIL_ADDRESS.matcher(etEmail).matches() -> {
+                    // Set error text
+                    binding.tilLoginEmail.error = "wrongEmail"
+                }
+                else -> {
+                    // Clear error text
+                    binding.tilLoginEmail.error = null
+                }
+            }
+        }
+
+        if (etPsw != null) {
+            if (etPsw.isEmpty()) {
+                // Set error text
+                binding.tilLoginPsw.error = "isEmpty"
+            }
+            else {
+                // Clear error text
+                binding.tilLoginPsw.error = null
+            }
+        }
     }
     // endregion
 }
